e8e73d2fb826b7493efba2d99cbe6b2c9ae6ffcf
Scratch <Jiamingw29@gmail.com>
2020-10-19 18:21:57 -0400

    shooter now generate csv data for debugging

--- Team6964/src/main/java/org/igutech/teleop/Modules/Shooter.java
+++ Team6964/src/main/java/org/igutech/teleop/Modules/Shooter.java
@@ -2,12 +2,19 @@ package org.igutech.teleop.Modules;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.igutech.auto.util.LoggingUtil;
 import org.igutech.teleop.Module;
 import org.igutech.teleop.Teleop;
 import org.igutech.utils.ButtonToggle;
 import org.igutech.utils.control.PIDController;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 
 @Config
@@ -24,11 +31,17 @@ public class Shooter extends Module {
     private PIDController pidController;
     private boolean veloControlActive = false;
     private ButtonToggle veloToggle;
-
+    PrintWriter pw;
+    ArrayList<Long> time = new ArrayList<>();
+    ArrayList<Double> velo = new ArrayList<>();
+    ArrayList<Double> power = new ArrayList<>();
+    ArrayList<Double> error = new ArrayList<>();
+    DcMotorEx leftMotor = (DcMotorEx) Teleop.getInstance().getHardware().getMotors().get("shooterLeft");
+    DcMotorEx rightMotor = (DcMotorEx) Teleop.getInstance().getHardware().getMotors().get("shooterRight");
     public Shooter() {
         super(500, "Shooter");
     }
-
+    long startTime;
     @Override
     public void init() {
         gamepadService = (GamepadService) Teleop.getInstance().getService("GamepadService");
@@ -38,10 +51,18 @@ public class Shooter extends Module {
         }, () -> {
         });
         veloToggle.init();
+        try {
+            pw = new PrintWriter(LoggingUtil.getLogFile("shooter.csv"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        startTime = System.currentTimeMillis();
+        pw.println("time,velocity,power,error,setpoint");
     }
 
     @Override
     public void loop() {
+
         veloToggle.loop();
         veloControlActive = veloToggle.getState();
         double manualPower = gamepadService.getAnalog(2, "right_stick_y");
@@ -49,9 +70,14 @@ public class Shooter extends Module {
             Teleop.getInstance().getHardware().getMotors().get("shooterLeft").setPower(manualPower);
             Teleop.getInstance().getHardware().getMotors().get("shooterRight").setPower(manualPower);
         } else if (veloControlActive) {
+            time.add(System.currentTimeMillis()-startTime);
+            velo.add(bulkRead.getShooterLeftVelo());
+
             pidController.setkP(pShooterLeft);
             pidController.updateSetpoint(motorVelo);
             double autoPower = pidController.update(bulkRead.getShooterLeftVelo());
+            power.add(autoPower);
+            error.add(motorVelo-bulkRead.getShooterLeftVelo());
             dashboardTelemetry.addData("autoPower",autoPower);
             Teleop.getInstance().getHardware().getMotors().get("shooterLeft").setPower(-autoPower);
             Teleop.getInstance().getHardware().getMotors().get("shooterRight").setPower(-autoPower);
@@ -63,10 +89,18 @@ public class Shooter extends Module {
 
         dashboardTelemetry.addData("manual", manualPower);
         dashboardTelemetry.addData("Target Velo", motorVelo);
-        dashboardTelemetry.addData("MotorLeft Velo", bulkRead.getShooterLeftVelo());
-        dashboardTelemetry.addData("MotorRight Velo", bulkRead.getShooterRightVelo());
+        dashboardTelemetry.addData("MotorLeft Velo", leftMotor.getVelocity());
+        dashboardTelemetry.addData("MotorRight Velo",rightMotor.getVelocity());
         dashboardTelemetry.addData("PID Active ", veloControlActive);
         dashboardTelemetry.update();
 
+    }
+
+    @Override
+    public void stop() {
+        for(int i=0;i<time.size();i++){
+            pw.println((time.get(i))+","+velo.get(i)+","+power.get(i)+","+error.get(i)+","+"1200");
+        }
+        pw.close();
     }
 }
