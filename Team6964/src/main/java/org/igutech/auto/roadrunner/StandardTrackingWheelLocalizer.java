06c6fcec134221e7c04bfae9e0ffa146c08723a4
Scratch <Jiamingw29@gmail.com>
2020-11-01 13:33:49 -0500

    set so left and right encoder values are printed

--- Team6964/src/main/java/org/igutech/auto/roadrunner/StandardTrackingWheelLocalizer.java
+++ Team6964/src/main/java/org/igutech/auto/roadrunner/StandardTrackingWheelLocalizer.java
@@ -32,6 +32,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 1.14173; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
+    //14.6385 last tuned value
     public static double LATERAL_DISTANCE = 14.67; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = -6.94; // in; offset of the lateral wheel
     public static double xMultipler = 1.01123596;
