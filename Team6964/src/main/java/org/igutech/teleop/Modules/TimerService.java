d45217fdb8350c0e79eba5a660775cae3d0c17db
Scratch <Jiamingw29@gmail.com>
2021-01-04 18:43:46 -0500

    single time events can only be registered if there are no other ones registered

--- Team6964/src/main/java/org/igutech/teleop/Modules/TimerService.java
+++ Team6964/src/main/java/org/igutech/teleop/Modules/TimerService.java
@@ -7,13 +7,14 @@ import org.igutech.utils.events.RepeatedTimerEvent;
 import org.igutech.utils.events.SingleTimerEvent;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class TimerService extends Service {
     private ArrayList<SingleTimerEvent> events = new ArrayList<>();
     private ArrayList<RepeatedTimerEvent> repeatedTimerEvents = new ArrayList<>();
-
     private long startTime;
-
+    private Queue<SingleTimerEvent> singleEventQueue = new LinkedList<>();
     public TimerService() {
         super("TimerService");
     }
@@ -25,13 +26,28 @@ public class TimerService extends Service {
 
     @Override
     public void loop() {
+        ArrayList<SingleTimerEvent> eventsToBeRemoved = new ArrayList<>();
         long current = System.currentTimeMillis() - startTime;
+//        for(SingleTimerEvent i:singleEventQueue){
+//            events.add(i);
+//        }
+        if(singleEventQueue.peek()!=null && events.size()==0){
+            events.add(singleEventQueue.peek());
+        }
+        singleEventQueue.clear();
 
         for (SingleTimerEvent e : events) {
-            if(!e.hasFired() && current>=e.getTime()){
+            if(!e.hasFired() && System.currentTimeMillis()>=e.getTime()){
                 e.fire();
+                eventsToBeRemoved.add(e);
             }
         }
+
+        for(SingleTimerEvent e:eventsToBeRemoved){
+            events.remove(e);
+        }
+        eventsToBeRemoved.clear();
+
         for(RepeatedTimerEvent e:repeatedTimerEvents){
             if(current>=e.getTime()){
                 e.fire();
@@ -41,14 +57,12 @@ public class TimerService extends Service {
     }
 
     public void registerSingleTimerEvent(int time, Callback m) {
-        events.add(new SingleTimerEvent(time, m));
+        System.out.println("event registered");
+        singleEventQueue.add(new SingleTimerEvent(time, m));
     }
 
     public void registerRepeatedTimerEvents(int time, Callback m) {
         repeatedTimerEvents.add(new RepeatedTimerEvent(time, m));
     }
-
-
-
 
 }
