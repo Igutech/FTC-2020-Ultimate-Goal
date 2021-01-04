1e64ae2ac9fcd0151a20e0b3919cab7bd7c106d5
Scratch <Jiamingw29@gmail.com>
2021-01-04 18:46:15 -0500

    split auto paths into separate files

--- Team6964/src/main/java/org/igutech/auto/FullRedAuto.java
+++ Team6964/src/main/java/org/igutech/auto/FullRedAuto.java
@@ -1,4 +1,32 @@
 package org.igutech.auto;
 
-public class FullRedAuto {
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.igutech.auto.paths.RedA;
+import org.igutech.auto.roadrunner.SampleMecanumDrive;
+
+import java.util.ArrayList;
+
+
+@Autonomous
+public class FullRedAuto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d startPose = new Pose2d(-60, -48, Math.toRadians(0));
+        ArrayList<Trajectory> trajectories = RedA.createTrajectory(drive, startPose);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        for (Trajectory trajectory : trajectories) {
+            drive.followTrajectory(trajectory);
+        }
+
+    }
 }
--- Team6964/src/main/java/org/igutech/auto/paths/RedA.java
+++ Team6964/src/main/java/org/igutech/auto/paths/RedA.java
@@ -1,4 +1,41 @@
 package org.igutech.auto.paths;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+
+import org.igutech.auto.roadrunner.SampleMecanumDrive;
+
+import java.util.ArrayList;
+
 public class RedA {
+    public static ArrayList<Trajectory> createTrajectory(SampleMecanumDrive drive, Pose2d start) {
+        ArrayList<Trajectory> trajectories = new ArrayList<>();
+        Trajectory dropGoal = drive.trajectoryBuilder(start)
+                .splineToConstantHeading(new Vector2d(0, -60), Math.toRadians(0.0))
+                .splineToConstantHeading(new Vector2d(0, -35), Math.toRadians(0.0))
+                .build();
+        Trajectory getRings = drive.trajectoryBuilder(dropGoal.end())
+                .splineToSplineHeading(new Pose2d(-5, -35, Math.toRadians(180)), Math.toRadians(0))
+                .splineToSplineHeading(new Pose2d(-15, -35, Math.toRadians(180)), Math.toRadians(0))
+                .build();
+        Trajectory shootRing = drive.trajectoryBuilder(getRings.end())
+                .splineToSplineHeading(new Pose2d(0, -35, Math.toRadians(0)), Math.toRadians(180))
+                .build();
+
+        Trajectory getSecondGoal = drive.trajectoryBuilder(shootRing.end())
+                .splineToSplineHeading(new Pose2d(-30, -25, Math.toRadians(180)), Math.toRadians(0))
+                .splineToSplineHeading(new Pose2d(-40, -25, Math.toRadians(180)), Math.toRadians(0))
+                .build();
+        Trajectory dropSecondGoal = drive.trajectoryBuilder(getSecondGoal.end())
+                .splineToSplineHeading(new Pose2d(0, -60, Math.toRadians(0)), Math.toRadians(180))
+                .build();
+
+        trajectories.add(dropGoal);
+        trajectories.add(getRings);
+        trajectories.add(shootRing);
+        trajectories.add(getSecondGoal);
+        trajectories.add(dropSecondGoal);
+        return trajectories;
+    }
 }
