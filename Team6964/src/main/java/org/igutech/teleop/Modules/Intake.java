01f5c70e6edab4d192e2b78279a6266d37c8743d
Scratch <Jiamingw29@gmail.com>
2021-01-06 19:24:42 -0500

    added servo to release intake; lift servo will now always go back to position 0 when intake is active

--- Team6964/src/main/java/org/igutech/teleop/Modules/Intake.java
+++ Team6964/src/main/java/org/igutech/teleop/Modules/Intake.java
@@ -4,9 +4,12 @@ import org.igutech.teleop.Module;
 import org.igutech.teleop.Teleop;
 import org.igutech.utils.ButtonToggle;
 
+import static org.igutech.teleop.Modules.Index.currentShooterServoLevel;
+
 public class Intake extends Module {
     GamepadService gamepadService;
     ButtonToggle xToggle;
+    ButtonToggle releaseIntake;
     public Intake() {
         super(200, "Intake");
     }
@@ -16,20 +19,41 @@ public class Intake extends Module {
         gamepadService=(GamepadService) Teleop.getInstance().getService("GamepadService");
         xToggle = new ButtonToggle(1,"x",()->{},()->{});
         xToggle.init();
+
+        releaseIntake = new ButtonToggle(1,"dpad_left",()->{
+            Teleop.getInstance().getHardware().getServos().get("releaseLiftServo").setPosition(0.2);
+        },()->{
+            Teleop.getInstance().getHardware().getServos().get("releaseLiftServo").setPosition(0.4);
+        });
+        Teleop.getInstance().getHardware().getServos().get("releaseLiftServo").setPosition(0.4);
+
+        releaseIntake.init();
+    }
+
+    @Override
+    public void start() {
+        Teleop.getInstance().getHardware().getServos().get("releaseLiftServo").setPosition(0.2);
+
     }
 
     @Override
     public void loop() {
         double power = gamepadService.getAnalog(1,"left_trigger");
         if(power>0.1){
+            if(currentShooterServoLevel !=0){
+                Teleop.getInstance().getHardware().getServos().get("liftServo").setPosition(0.78);
+
+            }
             power=-1;
         }
         if(xToggle.getState()){
-            power = power*-1;
+            power = gamepadService.getAnalog(1,"left_trigger");
         }
+
         Teleop.getInstance().getHardware().getMotors().get("intake").setPower(power);
 
         xToggle.loop();
+        releaseIntake.loop();
 
     }
 }
