c496f4840323f63575d4c56d48177ac1ad8bfcfc
Scratch <Jiamingw29@gmail.com>
2021-01-18 18:54:19 -0500

    updated roadrunner pid values for updated robot

--- Team6964/src/main/java/org/igutech/auto/roadrunner/DriveConstants.java
+++ Team6964/src/main/java/org/igutech/auto/roadrunner/DriveConstants.java
@@ -42,7 +42,7 @@ public class DriveConstants {
     public static double WHEEL_RADIUS = 2; // in
     public static double GEAR_RATIO = 1.2; // output (wheel) speed / input (motor) speed
     //physical is 17.6
-    public static double TRACK_WIDTH = 15.9; // in
+    public static double TRACK_WIDTH = 14.3; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -50,9 +50,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.014;
-    public static double kA = 0.0025;
-    public static double kStatic = 0.002;
+    public static double kV = 0.0137;
+    public static double kA = 0.0047;
+    public static double kStatic = 0.05;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
