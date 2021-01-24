97cbe69b5f8d6b0c5ab1c4fa68275c6046bf7a4c
testing290 <75059354+testing290@users.noreply.github.com>
2021-01-23 20:03:25 -0500

    removed the getters from BulkRead

--- Team6964/src/main/java/org/igutech/teleop/Modules/BulkRead.java
+++ Team6964/src/main/java/org/igutech/teleop/Modules/BulkRead.java
@@ -3,6 +3,7 @@ package org.igutech.teleop.Modules;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.igutech.config.Hardware;
 import org.igutech.teleop.Service;
 import org.igutech.teleop.Teleop;
 
@@ -10,13 +11,7 @@ import java.util.List;
 
 public class BulkRead extends Service {
 
-    private DcMotorEx frontLeft, frontRight, backLeft, backRight, frontShooter, backShooter; // Motor Objects
-    private long frontLeftTick, frontRightTick, backLeftTick, backRightTick, frontShooterTick, backShooterTick; // Encoder Values
-
-
-    private double frontLeftVelo, frontRightVelo, backLeftVelo, backRightVelo, frontShooterVelo, backShooterVelo;
     List<LynxModule> allHubs;
-
     public BulkRead() {
         super("BulkRead");
     }
@@ -24,12 +19,6 @@ public class BulkRead extends Service {
     @Override
     public void init() {
 
-        frontLeft = (DcMotorEx) Teleop.getInstance().getHardware().getMotors().get("frontleft");  // Configure the robot to use these 4 motor names,
-        frontRight = (DcMotorEx) Teleop.getInstance().getHardware().getMotors().get("frontright");  // or change these strings to match your existing Robot Configuration.
-        backLeft = (DcMotorEx) Teleop.getInstance().getHardware().getMotors().get("backleft");
-        backRight = (DcMotorEx) Teleop.getInstance().getHardware().getMotors().get("backright");
-        frontShooter =(DcMotorEx)Teleop.getInstance().getHardware().getMotors().get("frontshooter");
-        backShooter =(DcMotorEx)Teleop.getInstance().getHardware().getMotors().get("backshooter");
         allHubs = Teleop.getInstance().hardwareMap.getAll(LynxModule.class);
         for (LynxModule module : allHubs) {
             module.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
@@ -42,59 +31,7 @@ public class BulkRead extends Service {
             module.clearBulkCache();
         }
 
-        frontLeftTick = frontLeft.getCurrentPosition();   // Uses 1 bulk-read to obtain ALL the motor data
-        frontRightTick = frontRight.getCurrentPosition();   // There is no penalty for doing more `get` operations in this cycle,
-        backLeftTick = backLeft.getCurrentPosition();   // but they will return the same data.e4 = m4.getCurrentPosition();
-        backRightTick = backRight.getCurrentPosition();
-        frontShooterTick = frontShooter.getCurrentPosition();
-        backShooterTick = backShooter.getCurrentPosition();
-
-        frontLeftVelo = frontLeft.getVelocity();
-        frontRightVelo = frontRight.getVelocity();
-        backLeftVelo = backLeft.getVelocity();
-        backRightVelo = backRight.getVelocity();
-        frontShooterVelo = frontShooter.getVelocity();
-        backShooterVelo = backShooter.getVelocity();
-
     }
 
-    public double getFrontShooterVelo() {
-        return frontShooterVelo;
-    }
 
-    public double getBackShooterVelo() {
-        return backShooterVelo;
-    }
-
-    public long getLeftOdoTick() {
-        return frontLeftTick;
-    }
-
-    public long getRightOdoTick() {
-        return frontRightTick;
-    }
-
-    public long getStrafeOdoTick() {
-        return backLeftTick;
-    }
-
-    public long getBackRightTicks() {
-        return backRightTick;
-    }
-
-    public double getFrontLeftVelo() {
-        return frontLeftVelo;
-    }
-
-    public double getFrontRightVelo() {
-        return frontRightVelo;
-    }
-
-    public double getBackLeftVelo() {
-        return backLeftVelo;
-    }
-
-    public double getBackRightVelo() {
-        return backRightVelo;
-    }
 }
