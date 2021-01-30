87ea1622d6ce77465b3b22c035cab213d61d6aae
Scratch <Jiamingw29@gmail.com>
2021-01-30 18:38:42 -0500

    changed timeout for rr to 1.0 instead of 0.5

--- Team6964/src/main/java/org/igutech/auto/roadrunner/SampleMecanumDrive.java
+++ Team6964/src/main/java/org/igutech/auto/roadrunner/SampleMecanumDrive.java
@@ -100,7 +100,7 @@ public class SampleMecanumDrive extends MecanumDrive {
 
         constraints = new MecanumConstraints(BASE_CONSTRAINTS, TRACK_WIDTH);
         follower = new HolonomicPIDVAFollower(TRANSLATIONAL_PID, TRANSLATIONAL_PID, HEADING_PID,
-                new Pose2d(0.5, 0.5, Math.toRadians(5.0)), 0.5);
+                new Pose2d(0.5, 0.5, Math.toRadians(5.0)), 1.0);
 
         poseHistory = new ArrayList<>();
 
