d8a44f6d303d94b74cbd67b275b1df81177b74eb
Scratch <Jiamingw29@gmail.com>
2021-01-30 18:40:38 -0500

    temp solution to testing the 2nd intake motor

--- Team6964/src/main/java/org/igutech/teleop/Modules/Intake.java
+++ Team6964/src/main/java/org/igutech/teleop/Modules/Intake.java
@@ -53,6 +53,7 @@ public class Intake extends Module {
         }
 
         Teleop.getInstance().getHardware().getMotors().get("intake").setPower(power);
+        Teleop.getInstance().getHardware().getMotors().get("frontshooter").setPower(power);
 
         xToggle.loop();
         releaseIntake.loop();
