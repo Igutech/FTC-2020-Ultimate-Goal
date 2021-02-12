0ac84f05ce4fa1c2bd4c722629dd689a31e4941d
Scratch <Jiamingw29@gmail.com>
2021-02-11 23:10:56 -0500

    changed wobble goal lift servo positions

--- Team6964/src/main/java/org/igutech/teleop/Modules/WobbleGoalGrabber.java
+++ Team6964/src/main/java/org/igutech/teleop/Modules/WobbleGoalGrabber.java
@@ -4,23 +4,29 @@ import org.igutech.teleop.Module;
 import org.igutech.teleop.Teleop;
 import org.igutech.utils.ButtonToggle;
 
+import java.util.HashMap;
+
 public class WobbleGoalGrabber extends Module {
     private GamepadService gamepadService;
     private ButtonToggle wobbleGoalLift;
     private ButtonToggle wobbleGoalServo;
+    private int wobbleGoalLiftServoPosition = 0;
+    private HashMap<Integer, Double> wobbleGoalLiftPositions;
+
     public WobbleGoalGrabber() {
         super(50, "WobbleGoalGrabber");
     }
 
     @Override
     public void init() {
-        Teleop.getInstance().getHardware().getServos().get("wobbleGoalLift").setPosition(0);
+        Teleop.getInstance().getHardware().getServos().get("wobbleGoalLift").setPosition(0.15);
+        Teleop.getInstance().getHardware().getServos().get("wobbleGoalServo").setPosition(0.25);
 
         gamepadService = (GamepadService) Teleop.getInstance().getService("GamepadService");
         wobbleGoalLift = new ButtonToggle(1, "dpad_left", () -> {
-            Teleop.getInstance().getHardware().getServos().get("wobbleGoalLift").setPosition(1);
+            handleWobbleGoalLift();
         }, () -> {
-            Teleop.getInstance().getHardware().getServos().get("wobbleGoalLift").setPosition(0);
+            handleWobbleGoalLift();
         });
 
         wobbleGoalServo = new ButtonToggle(1, "dpad_right", () -> {
@@ -31,7 +37,24 @@ public class WobbleGoalGrabber extends Module {
         wobbleGoalLift.init();
         wobbleGoalServo.init();
 
-        Teleop.getInstance().getHardware().getServos().get("wobbleGoalServo").setPosition(0.25);
+        wobbleGoalLiftPositions = new HashMap<>();
+        //transport, pickup, transport, dump
+        wobbleGoalLiftPositions.put(0, 0.15);
+        wobbleGoalLiftPositions.put(1, 1.0);
+        wobbleGoalLiftPositions.put(2, 0.15);
+        wobbleGoalLiftPositions.put(3, 0.4);
+
+
+    }
+
+    private void handleWobbleGoalLift() {
+        wobbleGoalLiftServoPosition++;
+        if (wobbleGoalLiftServoPosition > 3) {
+            wobbleGoalLiftServoPosition = 0;
+        }
+
+        Teleop.getInstance().getHardware().getServos().get("wobbleGoalLift").setPosition(wobbleGoalLiftPositions.get(wobbleGoalLiftServoPosition));
+
 
     }
 
