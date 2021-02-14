939ae64e8673b7ee051008a9f3c29226f2e72766
testing290 <Jiamingw29@gmail.com>
2021-02-13 19:26:55 -0500

    automate a bit of the wobble goal mechanism

--- Team6964/src/main/java/org/igutech/teleop/Modules/WobbleGoalGrabber.java
+++ Team6964/src/main/java/org/igutech/teleop/Modules/WobbleGoalGrabber.java
@@ -7,10 +7,11 @@ import org.igutech.utils.ButtonToggle;
 import java.util.HashMap;
 
 public class WobbleGoalGrabber extends Module {
+    private TimerService timerService;
     private GamepadService gamepadService;
     private ButtonToggle wobbleGoalLift;
     private ButtonToggle wobbleGoalServo;
-    private int wobbleGoalLiftServoPosition = 0;
+    private int wobbleGoalLiftServoPosition = 1;
     private HashMap<Integer, Double> wobbleGoalLiftPositions;
 
     public WobbleGoalGrabber() {
@@ -22,6 +23,7 @@ public class WobbleGoalGrabber extends Module {
         Teleop.getInstance().getHardware().getServos().get("wobbleGoalLift").setPosition(0.15);
         Teleop.getInstance().getHardware().getServos().get("wobbleGoalServo").setPosition(0.25);
 
+        timerService = (TimerService) Teleop.getInstance().getService("TimerService");
         gamepadService = (GamepadService) Teleop.getInstance().getService("GamepadService");
         wobbleGoalLift = new ButtonToggle(1, "dpad_left", () -> {
             handleWobbleGoalLift();
@@ -44,19 +46,43 @@ public class WobbleGoalGrabber extends Module {
         wobbleGoalLiftPositions.put(2, 0.15);
         wobbleGoalLiftPositions.put(3, 0.4);
 
-
     }
 
     private void handleWobbleGoalLift() {
-        wobbleGoalLiftServoPosition++;
         if (wobbleGoalLiftServoPosition > 3) {
             wobbleGoalLiftServoPosition = 0;
         }
-
         Teleop.getInstance().getHardware().getServos().get("wobbleGoalLift").setPosition(wobbleGoalLiftPositions.get(wobbleGoalLiftServoPosition));
+        switch (wobbleGoalLiftServoPosition) {
+            case 0:
+                wobbleGoalLiftServoPosition++;
+                break;
+            case 1:
+                timerService.registerUniqueTimerEvent(300, "Wobble", () -> {
+                    Teleop.getInstance().getHardware().getServos().get("wobbleGoalServo").setPosition(0.25);
+                    wobbleGoalLiftServoPosition++;
+                });
+                break;
+            case 2:
+                wobbleGoalLiftServoPosition++;
+                break;
+            case 3:
+                timerService.registerUniqueTimerEvent(1000, "Wobble", () -> {
+                    Teleop.getInstance().getHardware().getServos().get("wobbleGoalServo").setPosition(25);
+                    wobbleGoalLiftServoPosition=0;
+                    timerService.registerUniqueTimerEvent(1000, "Wobble", () -> {
+                        Teleop.getInstance().getHardware().getServos().get("wobbleGoalLift").setPosition(wobbleGoalLiftPositions.get(wobbleGoalLiftServoPosition));
+                        wobbleGoalLiftServoPosition++;
+                        System.out.println();
+                    });
+                });
+                break;
+
+        }
 
 
     }
+
 
     @Override
     public void loop() {
