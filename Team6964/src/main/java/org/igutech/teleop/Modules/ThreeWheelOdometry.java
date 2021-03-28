888505fa793ba861f0fa1e390a4a6efd8dfd9281
Wang Jiaming <Jiamingw29@gmail.com>
2021-03-27 21:14:40 -0400

    now using the standard odo math instead of rr's

--- Team6964/src/main/java/org/igutech/teleop/Modules/ThreeWheelOdometry.java
+++ Team6964/src/main/java/org/igutech/teleop/Modules/ThreeWheelOdometry.java
@@ -1,16 +1,22 @@
 package org.igutech.teleop.Modules;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-import org.igutech.auto.roadrunner.SampleMecanumDrive;
-import org.igutech.auto.roadrunner.StandardTrackingWheelLocalizer;
+import org.igutech.auto.util.Encoder;
 import org.igutech.teleop.Module;
 import org.igutech.teleop.Teleop;
 
 
 public class ThreeWheelOdometry extends Module {
     private Pose2d myPose = new Pose2d(0, 0, 0);
-    private StandardTrackingWheelLocalizer localizer;
+    double TRACK_WIDTH = 14.5948;
+    double prevstrafe = 0;
+    double prevvert = 0;
+    double prevheading;
+    double STRAFE_WIDTH = -6.94; //center of the robot to the perpendicular wheel
+    private Encoder leftEncoder, rightEncoder, strafeEncoder;
 
     public ThreeWheelOdometry() {
         super(Integer.MAX_VALUE - 1000, "ThreeWheelOdometry");
@@ -18,24 +24,57 @@ public class ThreeWheelOdometry extends Module {
 
     @Override
     public void init() {
-        localizer = new StandardTrackingWheelLocalizer(Teleop.getInstance().getHardware().getHardwareMap());
+        leftEncoder = new Encoder((DcMotorEx) Teleop.getInstance().getHardware().getMotors().get("frontleft"));
+        rightEncoder = new Encoder((DcMotorEx) Teleop.getInstance().getHardware().getMotors().get("backleft"));
+        strafeEncoder = new Encoder((DcMotorEx) Teleop.getInstance().getHardware().getMotors().get("frontright"));
+        leftEncoder.setDirection(Encoder.Direction.REVERSE);
     }
 
     @Override
     public void start() {
-        localizer.update();
     }
 
     @Override
     public void loop() {
-        localizer.update();
-        Teleop.getInstance().telemetry.addData("x",localizer.getPoseEstimate().getX());
-        Teleop.getInstance().telemetry.addData("y",localizer.getPoseEstimate().getY());
-        Teleop.getInstance().telemetry.addData("heading",localizer.getPoseEstimate().getHeading());
+        double vert = ((leftEncoder.getCurrentPosition() + rightEncoder.getCurrentPosition()) / 2.0);
+        double dvert = vert - prevvert;
+        double dtheta = getHeading() - prevheading;
+        double dstrafe = ((strafeEncoder.getCurrentPosition()) - prevstrafe) - (STRAFE_WIDTH * dtheta);
+
+        prevstrafe = strafeEncoder.getCurrentPosition();
+        prevvert = vert;
+
+        Vector2d offset = constantVeloTrack(dstrafe, prevheading, dvert, dtheta);
+        myPose = new Pose2d(myPose.getX() + offset.getX(), myPose.getY() + offset.getY(), getHeading());
+
+        prevheading = getHeading();
+
     }
 
+    public Vector2d constantVeloTrack(double dstrafe, double prevheading, double dvert, double dtheta) {
+        double sinterm = 0;
+        double costerm = 0;
+
+        if (dtheta == 0) {
+            sinterm = 1.0 - dtheta * dtheta / 6.0;
+            costerm = dtheta / 2.0;
+        } else {
+            sinterm = Math.sin(dtheta) / dtheta;
+            costerm = (1 - Math.cos(dtheta)) / dtheta;
+        }
+
+        return new Vector2d((dstrafe * sinterm) + (dvert * -costerm), (dstrafe * costerm) + (dvert * sinterm)).rotated(prevheading);
+    }
+
+    public double getHeading() {
+        return (angleWrap((rightEncoder.getCurrentPosition() - leftEncoder.getCurrentPosition()) / TRACK_WIDTH));
+    }
+
+
+    public double angleWrap(double angle) {
+        return (angle + (2 * Math.PI)) % (2 * Math.PI);
+    }
     public Pose2d getPose(){
-        return localizer.getPoseEstimate();
+        return myPose;
     }
-
 }
