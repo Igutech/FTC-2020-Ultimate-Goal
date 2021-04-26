ab8a6f5eff2f15b8f64369c9062c56feb106e9b6
Scratch <Jiamingw29@gmail.com>
2021-04-26 12:59:08 -0400

    set pose to auto start pose for teleop testing

--- Team6964/src/main/java/org/igutech/utils/PoseStorage.java
+++ Team6964/src/main/java/org/igutech/utils/PoseStorage.java
@@ -3,5 +3,5 @@ package org.igutech.utils;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 public class PoseStorage {
-    public static Pose2d currentPose = new Pose2d(0, 0, 0);
+    public static Pose2d currentPose = new Pose2d(-63, -35, Math.toRadians(0));
 }
